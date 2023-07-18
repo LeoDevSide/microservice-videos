e4d456e42673255c1bc044d90160a5c0417e32ee
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2023-07-18 00:35:37 +0000

    feat: add initial category DTOs to controllers

---
+++ src/nestjs/src/categories/dto/search-categories.dto.ts
@@ -0,0 +1,9 @@
+import { SortDirection } from '@me/micro-videos/src/@shared/domain';
+
+export class SearchCategoriesDTO {
+  page?: number;
+  per_page?: number;
+  sort?: string;
+  sort_dir?: SortDirection;
+  filter?: string;
+}
